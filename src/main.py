eafb63c3aaeea0e8021f9bb95e6110a466bec93d
Christian Kongsgaard <kongsgaard.c@gmail.com>
2023-03-04 17:05:32 +0100

    initial commit

---
+++ src/main.py
@@ -0,0 +1,42 @@
+import json
+from pathlib import Path
+
+import requests as requests
+
+
+def main(p: Path):
+    urls = json.loads(p.read_text())
+
+    data = []
+    for url in urls:
+        data.append(crawl(url))
+
+    out = Path("../index.json")
+    out.write_text(json.dumps(data))
+
+
+def crawl(url: str) -> dict:
+    response = requests.get(url)
+    response.raise_for_status()
+    data = response.json()
+    keys = list(data.keys())
+    data.update({"@id": url})
+
+    if "@context" in keys:
+        return data
+    else:
+        data.update({"@context": {
+            "@version": 1.1,
+            "schema": "https://schema.org/",
+            "name": "schema:Product",
+            "isAccessoryOrSparePartFor": {
+                "@id": "schema:isAccessoryOrSparePartFor",
+                "@container": "@type"
+            },
+        }})
+        return data
+
+
+if __name__ == "__main__":
+    p = Path("input.json")
+    main(p)
